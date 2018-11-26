c7db71b41791593ca6adc75311be65b9937a25d6
Arnaud Zelmar <arnaudzelmar@ma-3020-lx>
2018-11-26 12:02:21 +0100

    Récuperation des cartes

--- src/Card.java
+++ src/Card.java
@@ -1,11 +1,16 @@
 
 public class Card {
-	int CardId;
 	int FC1Crown;
 	String FC1Type;
 	int FC2Crown;
 	String FC2Type;
-	public Card() {
-		
+	int CardId;
+	public Card(int fC1Crown, String fC1Type, int fC2Crown, String fC2Type,int cardId) {
+		super();
+		CardId = cardId;
+		FC1Crown = fC1Crown;
+		FC1Type = fC1Type;
+		FC2Crown = fC2Crown;
+		FC2Type = fC2Type;
 	}
 }
--- src/CoreGame.java
+++ src/CoreGame.java
@@ -1,17 +1,38 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class CoreGame {
 	static final int twoPlayerCardCount = 24;
 	static final int threePlayerCardCount = 36;
 	static final int fourPlayerCardCount = 48;
 	List<Player> players;
+	List<Card> cardsList;
 	Color[] colorAvalaible;
 	int numberTotalKings;
 	public CoreGame()
 	{
 		this.players = new ArrayList<Player>();
+		this.cardsList = new ArrayList<>();
 		this.colorAvalaible = new Color[]{Color.pink,Color.yellow,Color.green,Color.blue};
+		try {
+		BufferedReader reader = new BufferedReader(new FileReader("dominos.csv"));
+		String line = null;
+		Scanner scanner = null;
+		reader.readLine();
+		while ((line = reader.readLine()) != null) {
+			scanner = new Scanner(line);
+			scanner.useDelimiter(",");
+			Card card = new Card(Integer.parseInt(scanner.next()),scanner.next(),Integer.parseInt(scanner.next()),scanner.next(),Integer.parseInt(scanner.next()));
+			cardsList.add(card);
+		}
+		reader.close();
+		}
+		catch(Exception e) {System.out.println(e);};
+		
 	}
 	public void initGame() {
 		int numberPlayers = 0;
