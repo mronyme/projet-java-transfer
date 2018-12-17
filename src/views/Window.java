ecbcb6cf81ecebfbff52e125d7fc1e00135a75b1
chaimaa <chaimaabentahar@gmail.com>
2018-12-17 11:46:10 +0100

    changement dans Windows.java

--- src/views/Window.java
+++ src/views/Window.java
@@ -14,79 +14,75 @@ public class Window extends JFrame implements ActionListener {
     /**
      *
      */
-    private static final long serialVersionUID = 1L;
-    private JButton b1;
-    private JLabel j1, j2;
-    private Font f = new Font("Verdana", Font.PLAIN, 50);
-    private Font e = new Font("Bold", Font.PLAIN, 30);
-
-    private JLabel txt1;
-    private JRadioButton br1, br2, br3;
-    private Border lineBorder = new LineBorder(Color.BLACK, 2);
-    JPanel panelCont = new JPanel();
-    private CoreGame game;
+	 private static final long serialVersionUID = 1L;
+	    private JButton b1;
+	    private JLabel j1, j2;
+	    private Font f = new Font("Verdana", Font.PLAIN, 30);
+	private ButtonGroup bg;
+	    private JLabel txt1;
+	    private JRadioButton br1, br2, br3;
+	    private Border lineBorder = new LineBorder(Color.BLACK, 2);
+	    private JPanel panneau21, panneau22,panneau23;
+	    JPanel panelCont = new JPanel();
+	    private static CoreGame game;
+	    
     public Window(CoreGame game) {
+
         super("KingDomino");
     	this.game = game;
-        setSize(1000, 1000);
-        CardLayout cl = new CardLayout();
-        panelCont.setLayout(cl);
-        Container cp = new JPanel(); // à supprimer 
-        cp.setBackground(Color.black);
-        JPanel panneau1 = new JPanel();
-        JPanel panneau2 = new JPanel();
-        JPanel panneau21 = new JPanel();
-        JPanel panneau22 = new JPanel();
-
-        cp.add(panneau1, BorderLayout.NORTH);
-        cp.add(panneau2, BorderLayout.CENTER);
-
-        // cp.add(panneau22,BorderLayout.SOUTH);
-
-        panneau1.setPreferredSize(new Dimension(300, 200));
-        // panneau21.setPreferredSize(new Dimension(100,100));
-        //panneau2.setPreferredSize(new Dimension(500,500));
-        panneau2.add(panneau21);
-        panneau2.add(panneau22);
-
-        panneau1.setBackground(Color.cyan);
-        txt1 = new JLabel("Bienvenue dans KingDomino ");
-        txt1.setFont(f);
-        panneau1.add(txt1);
-
-        panneau2.setLayout(new GridLayout(3, 1));
-        panneau2.setBackground(Color.cyan);
-        j1 = new JLabel("Nombres de joueurs ");
-        j1.setFont(e);
-        panneau21.add(j1);
-        br1 = new JRadioButton("2");
-        br2 = new JRadioButton("3");
-        br3 = new JRadioButton("4");
-        panneau21.add(br1);
-        panneau21.add(br2);
-        panneau21.add(br3);
-
-
-        ButtonGroup bg = new ButtonGroup();
-
-        bg.add(br1);
-        bg.add(br2);
-        bg.add(br3);
-
-
-        panneau21.setBackground(Color.cyan);
-        panneau22.setBackground(Color.cyan);
-        b1 = new JButton("Play");
-        panneau22.add(b1);
-        b1.addActionListener(this);
-        panneau2.setBorder(lineBorder);
+    
+      
+   	 
+   		Container contenu = this.getContentPane();
+   		setSize(400, 300);
+   		contenu.setLayout(new BorderLayout());
+   		
+   		
+   		
+   		 panneau21 = new JPanel();
+   		 panneau22 = new JPanel();
+   		 panneau23 = new JPanel();
+   		
+   		
+  
+   		panneau21.setLayout(new FlowLayout());
+   		
+   		contenu.setLayout(new BorderLayout());
+   		contenu.add(panneau21,BorderLayout.NORTH);
+   		contenu.add(panneau22,BorderLayout.CENTER);
+   		contenu.add(panneau23,BorderLayout.SOUTH);
+   		   
+   	    panneau22.setLayout(new GridLayout(3,1));
+   	 
+   	    txt1=new JLabel("Nombre de joueur");
+   		txt1.setFont(f);
+   		panneau21.add(txt1);
+   		
+   		 br1 =new JRadioButton ("2 joueurs");
+   		 br2 =new JRadioButton ("3 joueurs");
+   		 br3 =new JRadioButton ("4 joueurs");
+   		panneau22.add(br1);
+   		panneau22.add(br2);
+   		panneau22.add(br3);
+   		 bg =new ButtonGroup();
+   		
+   		bg.add(br1);		
+   		bg.add(br2);	
+   		bg.add(br3);	
+   	
+   		panneau23.setLayout(new FlowLayout());
+   		 b1 =new JButton("Play");
+   	
+   		panneau23.add(b1);
+       
+   	 b1.addActionListener(this);
+        panneau22.setBorder(lineBorder);
         
-        panelCont.add(cp,"1");
-        cl.show(panelCont, "1");
-        add(panelCont);
+      
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        pack();
-        setVisible(true);
+      
+      
+
 
     }
     public void renderMenu()
@@ -95,11 +91,11 @@ public class Window extends JFrame implements ActionListener {
     }
     public void renderGeneralBoard()
     {	
-    	// Affiche la fenetre avec tous les territoires actualisé ( appellée à chaque début de tour de jeu) 
+    	// Affiche la fenetre avec tous les territoires actualisï¿½ ( appellï¿½e ï¿½ chaque dï¿½but de tour de jeu) 
     }
     public void renderSelectionCard()
     {
-    	// Affiche la fenetre de sélection des cartes à l'aide des rois ( appellée à chaque début de tour de jeu)
+    	// Affiche la fenetre de sï¿½lection des cartes ï¿½ l'aide des rois ( appellï¿½e ï¿½ chaque dï¿½but de tour de jeu)
     }
     public void renderPlayerBoard()
     {
