36cd60f072386b908937d42fae29974aed517d08
Baptiste.L <mronyme@users.noreply.github.com>
2019-01-02 18:34:20 +0100

    Merge branch 'master' of https://github.com/mronyme/projet-java
    # Conflicts:
    #	bin/.gitignore

--- src/views/Panneau.java
+++ src/views/Panneau.java
@@ -12,15 +12,15 @@ public class Panneau extends JPanel {
 
     public void paintComponent(Graphics g) {
 
-        //Vous verrez cette phrase chaque fois que la méthode sera invoquée
+        //Phrase apparaissant chaque fois que la méthode sera invoquée
         System.out.println("Je suis exécutée !");
-
+/*
         int x1 = this.getWidth() / 4;
         int y1 = this.getHeight() / 4;
 
         // Création d'un rectangle redimensionable
         g.fillRect(x1, y1, this.getWidth() / 2, this.getHeight() / 2);
-
+*/
         // Insertion du background
         try {
             Image img = ImageIO.read(new File("fond.jpg"));
