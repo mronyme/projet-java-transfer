3ed67acb2ca3b6f769d010c629c3c238d70cd7ad
Baptiste.L <mronyme@users.noreply.github.com>
2019-01-03 14:52:25 +0100

    Merge branch 'master' of https://github.com/mronyme/projet-java
    # Conflicts:
    #	bin/.gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
 import core.CoreGame;
-import views.Window2;
 import views.crashtest;
 
 public class Main {
@@ -8,7 +7,7 @@ public class Main {
 
 		CoreGame game = new CoreGame();
 		//Window window = new Window(game);
-		Window2 window = new Window2(game);
+        //Window2 window = new Window2(game);
 		crashtest window2 = new crashtest();
 		// Syntaxe : nom_classe nom_objet = new nom_constructeur(paramètres) ;
 	}
