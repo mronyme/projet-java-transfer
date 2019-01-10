8e6b2cb06d82f475443dff985346cc263757aeea
Baptiste.L <mronyme@users.noreply.github.com>
2019-01-10 12:13:44 +0100

    code prêt pour la Geek'night

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ public class Main {
 	public static void main(String[] args) {
 
 		CoreGame game = new CoreGame();
-		//Window window = new Window(game);
-        //Window2 window = new Window2(game);
         IHM window2 = new IHM(game);
 		// Syntaxe : nom_classe nom_objet = new nom_constructeur(paramètres) ;
 	}
