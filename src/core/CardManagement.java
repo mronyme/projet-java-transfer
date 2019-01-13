74fe4db858a0db45518326ab58518d641dec8648
AZelmar <arnaud.zelmar@isep.fr>
2019-01-13 21:16:45 +0100

    FIX

--- src/core/CardManagement.java
+++ src/core/CardManagement.java
@@ -129,17 +129,21 @@ public class CardManagement {
 		public void discardCards(int numberPlayer)
 		{
 			Random rand = new Random();
-			/*
-			for(int i = 0;i < 48-numberPlayer*12;i++)
+			if(game.getGameOptions().get("bigDuelOption") == false)
 			{
-				int n = (int )(Math.random() * cardsList.size());
-				cardsList.remove(n);
-			}*/
+				for(int i = 0;i < 48-numberPlayer*12;i++)
+				{
+					int n = (int )(Math.random() * cardsList.size());
+					cardsList.remove(n);
+			}
+			}
+			/*
 			for(int i = 0;i < 48-8;i++)
 			{
 				int n = (int )(Math.random() * cardsList.size());
 				cardsList.remove(n);
 			}
+			*/
 		}
 	    // -------> Inutile pour vous <-------
 		// Cette fonction pioche n cartes et les regroupent dans une liste de fa�on ordonn�e
